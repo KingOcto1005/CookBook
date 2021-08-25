60d3828dde4e077d949269d95a54f3064e6eba61
KingOcto1005 <opaloctopus1005@gmail.com>
2021-08-24 21:17:50 -0400

    service-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    ServiceListComponent
+  ]
 })
 export class AppModule { }
